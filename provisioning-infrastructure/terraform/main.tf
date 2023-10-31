9de51db4bb1aae167b9c788d3aa441ec30b1a28c
ziadali001 <zma.cee@gmail.com>
2023-10-31 16:43:34 +0200

    Creating EKS Cluster

--- provisioning-infrastructure/terraform/main.tf
+++ provisioning-infrastructure/terraform/main.tf
@@ -103,3 +103,16 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
   subnet_id = aws_subnet.dpp-public-subnet-02.id 
   route_table_id = aws_route_table.dpp-public-rt.id   
 }
+
+
+  module "sg_eks" {
+    source = "./sg_eks"
+    vpc_id = aws_vpc.dpp-vpc.id
+  }
+
+  module "eks" {
+      source      = "./eks"
+      vpc_id      = aws_vpc.dpp-vpc.id
+      subnet_ids  = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+      sg_ids      = module.sg_eks.security_group_public
+  }
